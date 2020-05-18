311d0dfff6978140935cb3a09e25d505365e7e3a
fabrice1618 <fabrice1618@gmail.com>
2020-05-18 13:47:35 +0200

    template2

--- public/index.php
+++ public/index.php
@@ -1,12 +1,3 @@
 <?php
 
-use Router\Router;
-
-require '../vendor/autoload.php';
-
-$router = new Router($_GET['url']);
-
-$router->get('/', 'App\Controllers\ProfilController@index');
-$router->get('/profil/:id', 'App\Controllers\ProfilController@show');
-
-$router->run();
+print_r($_GET);
